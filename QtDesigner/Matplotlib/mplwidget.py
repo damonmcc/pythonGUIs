c6a371a0af6c075918fcda3b93a42b0368df3e0a
Damon McCullough <damonmcc1391@gmail.com>
2019-03-04 11:18:48 -0500

    QtDesigner with matplotlib example

--- QtDesigner/Matplotlib/mplwidget.py
+++ QtDesigner/Matplotlib/mplwidget.py
@@ -1,15 +1,22 @@
-# Python Qt4 bindings for GUI objects
+# Adapted from examples in "Matplotlib for Python Developers", Sandro Tosi, Packt Publishing
+# https://www.packtpub.com/sites/default/files/sample_chapters/7900-matplotlib-for-python-developers-sample-chapter-6-embedding-matplotlib-in-qt-4.pdf
+
+# Python Qt5 bindings for GUI objects
 from PyQt5 import QtGui
-# import the Qt4Agg FigureCanvas object, that binds Figure to
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, \
+    QPushButton
+# import the Qt5Agg FigureCanvas object, that binds Figure to
 # Qt4Agg backend. It also inherits from QWidget
-from matplotlib.backends.backend_qt4agg \
+from matplotlib.backends.backend_qt5agg \
     import FigureCanvasQTAgg as FigureCanvas
+
 # Matplotlib Figure object
 from matplotlib.figure import Figure
 
 
 class MplCanvas(FigureCanvas):
     """Class to represent the FigureCanvas widget"""
+
     def __init__(self):
         # setup Matplotlib Figure and Axis
         self.fig = Figure()
@@ -18,29 +25,23 @@ class MplCanvas(FigureCanvas):
         FigureCanvas.__init__(self, self.fig)
         # we define the widget as expandable
         FigureCanvas.setSizePolicy(self,
-                                   QtGui.QSizePolicy.Expanding,
-                                   QtGui.QSizePolicy.Expanding)
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
         # notify the system of updated policy
         FigureCanvas.updateGeometry(self)
 
 
-class MplWidget(QtGui.QWidget):
+class MplWidget(QWidget):
     """Widget defined in Qt Designer"""
 
     def __init__(self, parent=None):
         # initialization of Qt MainWindow widget
-        QtGui.QWidget.__init__(self, parent)
-
+        super().__init__()
         # set the canvas to the Matplotlib widget
         self.canvas = MplCanvas()
         # create a vertical box layout
-        self.vbl = QtGui.QVBoxLayout()
+        self.vbl = QVBoxLayout()
         # add mpl widget to vertical box
         self.vbl.addWidget(self.canvas)
         # set the layout to th vertical box
         self.setLayout(self.vbl)
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MplWidget()
-    sys.exit(app.exec_())

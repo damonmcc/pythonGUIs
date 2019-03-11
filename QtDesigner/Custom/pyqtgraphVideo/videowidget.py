8d53b964f587497173cc2fca0bc7b65f6fc0ede4
Damon McCullough <damonmcc1391@gmail.com>
2019-03-11 17:46:31 -0400

    Added pyqtgraph examples, starting custom tiff viewer

---
+++ QtDesigner/Custom/pyqtgraphVideo/videowidget.py
@@ -0,0 +1,22 @@
+
+
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget
+
+from pyqtgraph.widgets.RawImageWidget import RawImageWidget
+
+
+class VideoWidget(QWidget):
+    """Widget defined in Qt Designer"""
+
+    def __init__(self, parent=None):
+        # initialization of Qt MainWindow widget
+        super(VideoWidget, self).__init__(parent)
+        # set the "canvas" to the RawImage widget
+        self.rawImg = RawImageWidget(QWidget())
+        # create a vertical box layout
+        self.vbl = QVBoxLayout()
+        # add mpl widget to vertical box
+        self.vbl.addWidget(self.rawImg)
+        # set the layout to th vertical box
+        self.setLayout(self.vbl)
---
+++ QtDesigner/pyqtgraph/examples.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# PyQtGraph includes an extensive set of examples
+# This will start a launcher with a list of available examples.
+# Select an item from the list to view its source code and double-click an item to run the example.
+
+import pyqtgraph.examples
+pyqtgraph.examples.run()
